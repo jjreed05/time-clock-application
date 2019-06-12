33e32f71a36b05c724f57b8377588a4848ba0983
jonsirrine <sir15004@byui.edu>
2019-06-11 21:15:56 -0700

    cleaned up addUser

--- routes/users.js
+++ routes/users.js
@@ -11,200 +11,201 @@ const saltRounds = 10;
 const uri = 'mongodb+srv://admin:admin123@gps-time-afto7.mongodb.net/test?retryWrites=true';
 
 router.get('/hello/', function(req, res, next){
-   res.send('CRUD testing 4');
+	 res.send('CRUD testing 4');
 })
 
 router.post("/authenticate/", function(req, res, next){
-   const username = req.body.username.toString();
-   const password = req.body.password.toString();
+	 const username = req.body.username.toString();
+	 const password = req.body.password.toString();
 
-   mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
-      if (err) throw err;
+	 mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
+			if (err) throw err;
 
-      const collection = client.db("usersDb").collection("userInformation");
-      collection.findOne({
-         $or: [
-            { "username": username },
-            { "email": username }
-         ]
-      }, (error, user) => {
-         if (error) throw err;
-         if (!user)
-            return res.status(400).send("No user found");
-         if (!bcrypt.compareSync(password, user.password))
-            return res.status(400).send("Bad username/email combination");
-         res.send(user);
-     });
-     client.close();
-   });
+			const collection = client.db("usersDb").collection("userInformation");
+			collection.findOne({
+				 $or: [
+						{ "username": username },
+						{ "email": username }
+				 ]
+			}, (error, user) => {
+				 if (error) throw err;
+				 if (!user)
+						return res.status(400).send("No user found");
+				 if (!bcrypt.compareSync(password, user.password))
+						return res.status(400).send("Bad username/email combination");
+				 res.send(user);
+		 });
+		 client.close();
+	 });
 });
 
 // get all users in company
 router.get("/getCompanyUsers", function (req, res){
-   const company = req.query.company;
+	 const company = req.query.company;
 
-   mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
-      if (err) throw err;
+	 mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
+			if (err) throw err;
 
-      const collection = client.db("usersDb").collection("userInformation");
-      collection.find({ "company": company }, (error, users) => {
-         if (error) throw err;
-         if (!users)
-            return res.status(400).send("No company found");
-         res.send(users);
-      });
-      client.close();
-   });
+			const collection = client.db("usersDb").collection("userInformation");
+			collection.find({ "company": company }, (error, users) => {
+				 if (error) throw err;
+				 if (!users)
+						return res.status(400).send("No company found");
+				 res.send(users);
+			});
+			client.close();
+	 });
 })
 
 // get user by id
 router.get("/getUser", function(req, res){
-  const email = req.query.email;
-  
-  mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
-    if (err) throw err;
-    const collection = client.db("usersDb").collection("userInformation");
-    collection.findOne({ "email": email }, (error, user) => {
-      if (error) throw err;
-      if (!user)
-        return res.status(400).send("No user found");
-      res.send(user);
-    })
-    client.close();
-  });
+	const email = req.query.email;
+	
+	mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
+		if (err) throw err;
+		const collection = client.db("usersDb").collection("userInformation");
+		collection.findOne({ "email": email }, (error, user) => {
+			if (error) throw err;
+			if (!user)
+				return res.status(400).send("No user found");
+			res.send(user);
+		})
+		client.close();
+	});
 });
 
 //update user by email
 router.post("/updateUser", function(req, res){
-   const email = req.body.email;
-   const username = req.body.username;
-   const newEmail = req.body.email;
-   const company = req.body.company;
-   const password = bcrypt.hashSync(req.body.password, saltRounds);
-   const isAdmin = req.body.isAdmin;
-   let userObject = { username, password, newEmail, company, isAdmin };
+	 const email = req.body.email;
+	 const username = req.body.username;
+	 const newEmail = req.body.email;
+	 const company = req.body.company;
+	 const password = bcrypt.hashSync(req.body.password, saltRounds);
+	 const isAdmin = req.body.isAdmin;
+	 let userObject = { username, password, newEmail, company, isAdmin };
 
-   mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
-      if (err) throw err;
+	 mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
+			if (err) throw err;
 
-      const collection = client.db("usersDb").collection("userInformation");
-      collection.update({"email": email}, userObject, (error, result) => {
-         if (error) throw err;
-         if (!result)
-            return res.status(400).send("No user found");
-         res.send(result);
-      })
-   });
+			const collection = client.db("usersDb").collection("userInformation");
+			collection.update({"email": email}, userObject, (error, result) => {
+				 if (error) throw err;
+				 if (!result)
+						return res.status(400).send("No user found");
+				 res.send(result);
+			})
+	 });
 })
 
 //delete user by id
 router.delete("/deleteUser", function(req, res){
-   const userId = req.body.userId.toString();
-   res.send(userId);
+	 const userId = req.body.userId.toString();
+	 res.send(userId);
 
 
-   mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
-      if (err) throw err;
+	 mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
+			if (err) throw err;
 
-      const collection = client.db("usersDb").collection("userInformation");
-      collection.deleteOne({"_id": ObjectId(userId)}, (error, result) => {
-         if (error) throw error;
-         if (!result)
-            return res.status(400).send(false);
-         res.send(result);
-      })
-
-   });
+			const collection = client.db("usersDb").collection("userInformation");
+			collection.deleteOne({"_id": ObjectId(userId)}, (error, result) => {
+				 if (error) throw error;
+				 if (!result)
+						return res.status(400).send(false);
+				 res.send(result);
+			})
+	 });
 })
 
 router.post("/addUser", function(req, res){
-   const username = req.body.username;
-   const email = req.body.email;
-   const company = req.body.company;
-   const password = bcrypt.hashSync(req.body.password, saltRounds);
+	 const username = req.body.username;
+	 const email = req.body.email;
+	 const company = req.body.company;
+	 const password = bcrypt.hashSync(req.body.password, saltRounds);
 
-   let isAdmin = false;
-   let userObject = { username, password, email, company, isAdmin };
+	 let isAdmin = false;
+	 let userObject = { username, password, email, company, isAdmin };
 
 
-   // connect to atlas
-   mongoClient.connect(uri, { useNewUrlParser: true }, async (err, client) => {
-      if (err) throw err;
+	 // connect to atlas
+	 mongoClient.connect(uri, { useNewUrlParser: true }, async (err, client) => {
+		if (err) throw err;
 
-      // add user to the db
-      const userInformation = client.db("usersDb").collection("userInformation");
-      const companyInformation = client.db("usersDb").collection("companyInformation");
-      const timeTable = client.db("usersDb").collection("timeTable");
-      await userInformation.findOne(
-        {$or: [{ "username": username }, { "email": email }]}, function (err, user) {
+		// get collections
+		const userInformation = client.db("usersDb").collection("userInformation");
+		const companyInformation = client.db("usersDb").collection("companyInformation");
+		const timeTable = client.db("usersDb").collection("timeTable");
 
-          // is a new user
-          if (!user) {
-        
-            // determine if the company exists
-            await companyInformation.findOne({ "name": company } , (error, company) => {
-              if (err) throw error;
+		// find user
+		await userInformation.findOne(
+			{$or: [{ "username": username }, { "email": email }]}, function (err, user) {
 
-              // if company doesn't exist
-              if(!!company){
+				// is a new user
+				if (!user) {
+			
+					// determine if the company exists
+					await companyInformation.findOne({ "name": company } , (error, company) => {
+						if (err) throw error;
 
-                // new user should be created as admin
-                isAdmin = true;
-                userObject = { username, password, email, company, isAdmin };
+						// if company doesn't exist
+						if(!!company){
 
-                // new company should be created
-                let name = company;
-                let anotherObject = { name };
-                companyInformation.insertOne(anotherObject, (error, result) => {
-                  if (err) throw (err);
-                  userInformation.insertOne(userObject, function(err, result){
-                    if (err) throw err;
+							// new user should be created as admin
+							isAdmin = true;
+							userObject = { username, password, email, company, isAdmin };
 
-                    // set up the time table
-                    const userInformation = result.ops;
-                    const userId = result.insertedId;
-                    const isWorking = false;
-                    const time = [];
-                    const timeObj = { userId, isWorking, time };
+							// new company should be created
+							let name = company;
+							let anotherObject = { name };
+							companyInformation.insertOne(anotherObject, (error, result) => {
+								if (err) throw (err);
+								userInformation.insertOne(userObject, function(err, result){
+									if (err) throw err;
 
-                    // insert time table
-                    timeTable.insertOne(timeObj, function(err, result){
-                      if(err) throw err;
-                      res.send(userInformation);
-                      client.close();
-                    });
-                  });
-                });
-              } else {
-                companyInformation.insertOne(anotherObject, (error, result) => {
-                  if (err) throw (err);
-                  userInformation.insertOne(userObject, function(err, result){
-                    if (err) throw err;
+									// set up the time table
+									const userInformation = result.ops;
+									const userId = result.insertedId;
+									const isWorking = false;
+									const time = [];
+									const timeObj = { userId, isWorking, time };
 
-                    // set up the time table
-                    const userInformation = result.ops;
-                    const userId = result.insertedId;
-                    const isWorking = false;
-                    const time = [];
-                    const timeObj = { userId, isWorking, time };
+									// insert time table
+									timeTable.insertOne(timeObj, function(err, result){
+										if(err) throw err;
+										res.send(userInformation);
+										client.close();
+									});
+								});
+							});
 
-                    // insert time table
-                    timeTable.insertOne(timeObj, function(err, result){
-                      if(err) throw err;
-                      res.send(userInformation);
-                      client.close();
-                    });
-                  });
-                });
-              }
-            });
-          }
-          else {
-            res.status(400).send("User exists");
-            client.close();
-          }
-       });
-    });
+						// of company already exists
+						} else {
+
+							// only create the user (as non admin)
+							userInformation.insertOne(userObject, function(err, result){
+								if (err) throw err;
+
+								// still set up the time table
+								const userInformation = result.ops;
+								const userId = result.insertedId;
+								const isWorking = false;
+								const time = [];
+								const timeObj = { userId, isWorking, time };
+
+								// still insert time table
+								timeTable.insertOne(timeObj, function(err, result){
+									if(err) throw err;
+									res.send(userInformation);
+									client.close();
+								});
+							});
+						}
+					});
+				} else {
+					res.status(400).send("User exists");
+					client.close();
+				}
+		 }); // end finding user
+	}); // end mongo connect
 });
 
 
